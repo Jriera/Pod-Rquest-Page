9d6d58383114d02cea9c897082d76631f85bed9c
Jriera <jordiriera81@gmail.com>
2021-08-18 11:35:34 +0200

    js added

---
+++ formValidator.js
@@ -0,0 +1,8 @@
+document.getElementsByClassName("bt").addEventListener("click",function (){
+    let x = document.forms["subscription"]["email"].value;
+    let messenger = document.getElementsByClassName("error");
+    if(x===""){
+        messenger.innerHTML="Oops! Please add your email";
+        return false;
+    }
+});
